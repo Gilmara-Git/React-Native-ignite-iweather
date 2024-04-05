94390bb96fe80b53a6d589896bfca20636f9dd6d
Gilmara Pimentel <gilmarapq@hotmail.com>
2024-04-05 16:45:09 -0400

    test: use mockCityAPIResponse

--- src/services/__tests__/getCityByNameServices.test.ts
+++ src/services/__tests__/getCityByNameServices.test.ts
@@ -1,26 +1,15 @@
 import { api } from '@services/api';
 import { getCityByNameService } from '@services/getCityByNameService';
+import { mockCityAPIResponse } from '@__tests__/mocks/mockCityAPIResponse'
 
 
 describe('API Weather: getCityByNameService', ()=>{
     it('should return city details', async ()=>{
 
-        const apiResponse = {
-            id: '1',
-            name: 'São Paulo',
-            sys: {
-                country: 'BR'
-            },
-            coord:{
-                lat: 123,
-                lon: 456
-            }
-        };
+        jest.spyOn(api, "get").mockResolvedValue({data : mockCityAPIResponse});
 
-        jest.spyOn(api, "get").mockResolvedValue({data : apiResponse});
         const response = await getCityByNameService("São Paulo");
-        console.log(response, 'resposta da API')
-     
+        // console.log(response)
         expect(response.length).toBeGreaterThan(0);
     })
 })
