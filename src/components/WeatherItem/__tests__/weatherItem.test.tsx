c3c9f804722b77c8ca50cf6927cbfceaaf367c03
Gilmara Pimentel <gilmarapq@hotmail.com>
2024-04-09 21:40:09 -0400

    test: add test to WeatherItem component

---
+++ src/components/WeatherItem/__tests__/weatherItem.test.tsx
@@ -0,0 +1,27 @@
+
+import { render, screen } from '@testing-library/react-native';
+import { WeatherItem } from '@components/WeatherItem';
+import Rain from '@assets/cloud_rain_light.svg';
+describe('Component: WeatherItem', ()=>{
+    it('should show/find Title and Value on render', ()=>{
+     const {debug} =  render( <WeatherItem 
+            icon={Rain}
+            title='Rain probability'
+            value='2.05/Km'
+            />
+            )
+            
+        debug();
+
+        const title = screen.getByText('Rain probability');
+        const value = screen.getByText('2.05/Km');
+
+        expect(title).toBeTruthy();
+        expect(value).toBeTruthy();
+
+    });
+    
+  
+});
+
+
